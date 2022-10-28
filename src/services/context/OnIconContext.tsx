09ce4701b1b8797cadd420b059744e6360162459
Wilson Ibekason <wanbekgithub@gmail.com>
2022-10-28 13:48:18 +0100

    Add Icons Globally

--- src/services/context/OnIconContext.tsx
+++ src/services/context/OnIconContext.tsx
@@ -1,4 +1,8 @@
 import React from "react";
+import { BiSearchAlt, BiMenu } from "react-icons/bi";
+import { FiFacebook, FiInstagram } from "react-icons/fi";
+import { FaWhatsapp, FaPinterestP, FaLinkedinIn } from "react-icons/fa";
+import { BsTwitter } from "react-icons/bs";
 import { IconContextProps } from "./context.type";
 
 interface IconContextTypes {}
@@ -7,5 +11,20 @@ export const IconContext = React.createContext<any>({});
 export const IconContextProvider: React.FC<IconContextProps> = ({
   children,
 }) => {
-  return <IconContext.Provider value={{}}>{children}</IconContext.Provider>;
+  return (
+    <IconContext.Provider
+      value={{
+        BiSearchAlt,
+        BiMenu,
+        FiFacebook,
+        FiInstagram,
+        FaWhatsapp,
+        FaPinterestP,
+        FaLinkedinIn,
+        BsTwitter,
+      }}
+    >
+      {children}
+    </IconContext.Provider>
+  );
 };
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,6 +9,13 @@ module.exports = {
         cascadiacodePL: ["CascadiaCodePL", "monospace"],
         cascadiacodePLItalic: ["CascadiaCodePLItaic", "monospace"],
       },
+      colors: {
+        darkWhite: "#fefffe",
+        appGreen: "#5cb757",
+        appBlue: "#468ed5",
+        appRed: "#cc282c",
+        appPink: "#e59293",
+      },
       fontSize: {
         h1: "44px",
         h2: "36px",
