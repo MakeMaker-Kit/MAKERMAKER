1ec45f08ad1623e7cdf7644cfa0d6bd3802c5b12
wilsonibekason <wanbekgithub@gmail.com>
2022-10-30 15:25:37 +0100

    Complete add Testimonial Card

--- src/components/home/homecontents/testimonials/Testimonial.tsx
+++ src/components/home/homecontents/testimonials/Testimonial.tsx
@@ -25,17 +25,49 @@ const Testimonial = () => {
             </h2>
           </div>
           <div className={cx(`overflow-visible ${XFull} ${flexCenter}`)}>
-            <div className={cx(`h-52 w-[500px]`, `${containerWrapper} `)}>
-              <div className={`${boxFull} relative `}>
+            <div className={cx(`h-52 w-[500px]`, ` pt-16 relative`)}>
+              <div
+                className={`${boxFull} shadow-amber-800 shadow-lg border-t border-solid border-spacing-9 border-appPink rounded-lg px-5 pb-4`}
+              >
+                {/* Image VIewer */}
                 <div
                   className={cx(
-                    ` absolute  top-1 left-[45%] mx-auto`,
-                    `w-20 h-20 rounded-full border border-gray-500`
+                    ` absolute  top-3 left-[45%] mx-auto`,
+                    `w-20 h-20 rounded-full bg-gradient-to-r from-slate-900 via-appBlue to-red-100 z-[10]`,
+                    `p-1 `
                   )}
-                ></div>
-                <div
-                  className={cx(`pt-16 w-full`, `${containerWrapper} `)}
-                ></div>
+                >
+                  <div
+                    className={cx(
+                      `${boxFull} ${flexCenter} bg-white z-10 rounded-full`
+                    )}
+                  >
+                    <h1>Hello</h1>
+                  </div>
+                </div>
+                {/* content */}
+                <div className={cx(`w-full pt-10 `)}>
+                  <div className={cx(`h-full ${flexCol} space-y-6`)}>
+                    <p
+                      className={cx(
+                        `line-clamp-5 ${textCustom} ${mainLayout} text-xs text-center font-cascadiacodePLItalic font-semibold`
+                      )}
+                    >
+                      I have even had kids who would never speak up or engage in
+                      much of anything ask to come in at lunch to experiment!
+                      NOW that makes me happy!…
+                    </p>
+                    <div
+                      className={cx(
+                        `${flexCenter} ${flexRowCenter} space-x-2 ${mainLayout} ${textCustom} text-sm font-bold text-appBlue`
+                      )}
+                    >
+                      <h1>Alexandar Ibekason</h1>
+                      <div className="w-px h-5 bg-black" />
+                      <h1>Wizardomw</h1>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
--- src/types/global.types.ts
+++ src/types/global.types.ts
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export interface NavItemTypes {
   name: string;
   link: string;
