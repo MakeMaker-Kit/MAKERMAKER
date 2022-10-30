da06114a716389c4c2c5d0bec88d4ea872d774e9
wilsonibekason <wanbekgithub@gmail.com>
2022-10-30 19:07:41 +0100

    Complete add Faq card Layout

--- src/components/home/homecontents/homeFaqs/HomeFaqs.tsx
+++ src/components/home/homecontents/homeFaqs/HomeFaqs.tsx
@@ -9,7 +9,7 @@ import {
 const HomeFaqs = () => {
   const { themeWrapper, XFull, boxFull, containerWrapper } = themes;
   const { largeMarX, marX, padY } = themeWrapper;
-  const { flexCol, flexStart, flexRow } = flexLayout;
+  const { flexCol, flexStart, flexRow, flexCenter } = flexLayout;
   const {} = textStyles;
   return (
     <>
@@ -27,8 +27,39 @@ const HomeFaqs = () => {
               .map((i) => (
                 <div
                   key={i}
-                  className={`w-full md:w-full lg:w-five h-20 ${containerWrapper}`}
-                ></div>
+                  className={`w-full md:w-full lg:w-five h-40 ${containerWrapper} p-4`}
+                >
+                  <div className={cx(`${boxFull} ${flexCol} space-y-4`)}>
+                    <div
+                      className={cx(`h-36 ${XFull} ${containerWrapper} p-2`)}
+                    >
+                      <div className={`${boxFull} ${flexRow} items-center`}>
+                        <div className={`w-20 h-20 border rounded-full`}></div>
+                        <div
+                          className={cx(
+                            `w-seven max-w-seven h-full rounded-md`
+                          )}
+                        >
+                          <div
+                            className={cx(
+                              `${XFull} ${flexCol} flex  justify-center`
+                            )}
+                          >
+                            <div>
+                              <h1>Lorem ipsum dolor sit.</h1>
+                            </div>
+                            <div>
+                              <p>
+                                Lorem ipsum dolor sit amet consectetur
+                                adipisicing elit. Consectetur, in!
+                              </p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               ))}
           </div>
         </div>
