b01ae6666f676c59ad81c04e4abee6255eba275f
wilsonibekason <wanbekgithub@gmail.com>
2022-10-31 18:59:43 +0100

    Add ScrollToTop Button

--- src/components/appwrapper/Wrapper.tsx
+++ src/components/appwrapper/Wrapper.tsx
@@ -3,10 +3,12 @@ import { WrapperProps } from "./wrapper.types";
 import { Navbar, Footer } from "../applayout";
 import { wrapperStypes, themes } from "../../styles/themes/theme";
 import AuthLayout from "../modals/authmodal/AuthLayout";
+import ScrollToTop from "react-scroll-to-top";
 
 const Wrapper: React.FC<WrapperProps> = ({ children }) => {
   const { pageCenter } = wrapperStypes;
   const { boxFull } = themes;
+
   return (
     <>
       <div className={` bg-transparent`}>
@@ -20,6 +22,13 @@ const Wrapper: React.FC<WrapperProps> = ({ children }) => {
           {/* Footer */}
           <Footer />
         </div>
+        {/* ScrollTotop */}
+        <ScrollToTop
+          smooth
+          viewBox="0 0 24 24"
+          svgPath="M18 15l-6-6-6 6"
+          className={`flex items-center justify-center`}
+        />
       </div>
     </>
   );
