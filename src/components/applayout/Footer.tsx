77a03f2de852f3f77db93c722c476f0ca37b5db3
wilsonibekason <wanbekgithub@gmail.com>
2022-10-31 12:28:57 +0100

    Add Footer Container

--- src/components/applayout/Footer.tsx
+++ src/components/applayout/Footer.tsx
@@ -1,9 +1,17 @@
 import React from "react";
+import cx from "classnames";
+import { textStyles, themes, flexLayout } from "../../styles/themes/theme";
 
 const Footer = () => {
+  const {} = textStyles;
+  const { themeWrapper, boxFull, XFull, containerWrapper } = themes;
+  const { marX, padY } = themeWrapper;
+  const {} = flexLayout;
   return (
     <>
-      <div></div>
+      <div className={cx(`${marX + " " + padY}`)}>
+        <div className={`${boxFull} ${containerWrapper}`}></div>
+      </div>
     </>
   );
 };
