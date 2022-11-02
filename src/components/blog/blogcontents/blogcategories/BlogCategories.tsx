497a96009c8e332f08826ae97037104b8ca06c8e
Wilson Ibekason <wanbekgithub@gmail.com>
2022-11-02 10:54:28 +0100

    Complete add BlogCategories

--- src/components/blog/blogcontents/blogcategories/BlogCategories.tsx
+++ src/components/blog/blogcontents/blogcategories/BlogCategories.tsx
@@ -9,7 +9,14 @@ import {
 
 const BlogCategories = () => {
   const { flexCol, flexRow, flexRowCenterBetween, flexCenter } = flexLayout;
-  const { themeWrapper, containerWrapper, boxFull, XFull, transition } = themes;
+  const {
+    themeWrapper,
+    containerWrapper,
+    boxFull,
+    XFull,
+    transition,
+    transitionEase,
+  } = themes;
   const {} = themeWrapper;
   const { mainLayout, textCustom } = textStyles;
   const { ArrowRightIcon } = useIcon();
@@ -25,34 +32,34 @@ const BlogCategories = () => {
                   key={i}
                   className={cx(
                     `h-24 ${XFull}`,
-                    `rounded-lg shadow group ${transition} border border-dotted border-slate-300`
+                    `rounded-lg shadow group ${transitionEase} border border-dotted border-slate-300`
                   )}
                 >
                   <div
                     className={cx(
-                      `bg-center bg-cover bg-no-repeat bg-clip-border bg-origin-content max-w-full w-full h-full cursor-pointer rounded-lg group-hover:p-2`,
+                      `bg-center bg-cover bg-no-repeat bg-clip-border bg-origin-content max-w-full w-full h-full cursor-pointer rounded-lg group-hover:p-3`,
                       ` bg-[url('../assets/images/img2.webp')] bghover`
                     )}
                   >
                     <div className={cx(`relative ${boxFull}`)}>
                       <div
                         className={cx(
-                          `absolute top-[40%] w-full invisible group-hover:visible`,
+                          `absolute top-[35%] w-full invisible group-hover:visible`,
                           `${flexRowCenterBetween}`
                         )}
                       >
                         <div
                           className={cx(
-                            `${mainLayout} ${textCustom} text-sm font-semibold text-white font-gordita`
+                            `${mainLayout} ${textCustom} text-[1rem] font-semibold text-white font-gordita`
                           )}
                         >
                           <h3 className={cx(`tracking-wide`)}>Designing</h3>
                         </div>
                         <div
-                          className={`w-auto h-8 p-2 bg-orange text-white rounded-md`}
+                          className={`w-8 h-8 p-2 bg-orange text-white rounded-full`}
                         >
                           <div className={`${boxFull} ${flexCenter}`}>
-                            <ArrowRightIcon />
+                            <ArrowRightIcon size={20} />
                           </div>
                         </div>
                       </div>
