6599d91c7122245f376f583051969c645da4a2de
Wilson Ibekason <wanbekgithub@gmail.com>
2022-11-02 16:12:22 +0100

    Complete add BlogMoreProfile

--- src/components/blog/blogcontents/blogmore/BlogMoreProfile.tsx
+++ src/components/blog/blogcontents/blogmore/BlogMoreProfile.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import cx from "classnames";
+import { useIcon } from "../../../../hooks/dispatchContext";
+import { FaLinkedin, FaLinkedinIn } from "react-icons/fa";
+import MainButton from "../../../../hooks/button/mainBTN";
 import {
   textStyles,
   themes,
@@ -18,12 +21,13 @@ const BlogMoreProfile = () => {
     flexColCenter,
     flexCenter,
   } = flexLayout;
+  const { FaLinkedinIn } = useIcon();
   return (
     <>
       <div
         className={`px-4 py-8 bg-white border border-dotted border-gray-300 rounded-lg shadow`}
       >
-        <div className={`${boxFull} ${flexColCenter}`}>
+        <div className={`${boxFull} ${flexColCenter} gap-y-6`}>
           {/* image */}
           <div className={`h-24 w-24 border border-double rounded-full p-3`}>
             <div
@@ -53,9 +57,24 @@ const BlogMoreProfile = () => {
             </p>
           </div>
           {/*  */}
-          <ul className={`${flexRowCenter}`}>
-            <li></li>
+          <ul className={`${flexRowCenter} gap-x-4`}>
+            {Array(4)
+              .fill(0)
+              .map((i) => (
+                <li
+                  className={`h-10 w-10 p-2 border bg-gray-100 rounded-full`}
+                  key={i}
+                >
+                  <div className={`${boxFull} ${flexCenter}`}>
+                    <FaLinkedinIn />
+                  </div>
+                </li>
+              ))}
           </ul>
+          {/*  */}
+          <div className={`w-full max-w-five `}>
+            <MainButton isRounded={true}>View Profile</MainButton>
+          </div>
         </div>
       </div>
     </>
