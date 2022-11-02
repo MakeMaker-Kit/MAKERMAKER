fce42854a93f8e9b0afe6fa570ea312d21351e95
Wilson Ibekason <wanbekgithub@gmail.com>
2022-11-02 12:38:36 +0100

    Complete add BogContact

--- src/components/blog/blogcontents/blogcontact/BlogContact.tsx
+++ src/components/blog/blogcontents/blogcontact/BlogContact.tsx
@@ -5,13 +5,16 @@ import {
   flexLayout,
   textStyles,
 } from "../../../../styles/themes/theme";
+import Input from "../../../../hooks/Input";
+import Button from "../../../../hooks/button";
+import MainButton from "../../../../hooks/button/mainBTN";
 
 const BlogContact = () => {
   const { themeWrapper, boxFull, XFull, containerWrapper } = themes;
   const { mainMarX, largeMarX } = themeWrapper;
   const { flexRowCenterBetween, flexRow, flexCenter, flexRowCenter } =
     flexLayout;
-  const {} = textStyles;
+  const { mainLayout, textCustom } = textStyles;
   return (
     <>
       <div className={cx(`${boxFull}`)}>
@@ -20,15 +23,32 @@ const BlogContact = () => {
             {/* BlogContact Layout  */}
             <div
               className={cx(
-                `${XFull} h-48 py-10 px-20 rounded-lg shadow-md ${containerWrapper} `
+                `${XFull} h-28 py-10 px-20 rounded-lg shadow-md border bg-gray-100 `
               )}
             >
               <div
-                className={cx(`${boxFull} ${flexRowCenterBetween} bg-black`)}
+                className={cx(`${boxFull} ${flexRowCenterBetween} space-x-3 `)}
               >
-                <div></div>
-                <div></div>
-                <div></div>
+                <div className={cx(`w-two max-w-two`)}>
+                  <div
+                    className={`${flexCenter} ${XFull} ${textCustom} ${mainLayout} text-lg font-gordita break-words `}
+                  >
+                    <h1 className={`max-w-nine`}>
+                      Subscribe for our Blog Newsletter
+                    </h1>
+                  </div>
+                </div>
+                <div className={`w-six max-w-six`}>
+                  <div className={`${flexCenter} ${XFull}`}>
+                    <Input
+                      isRounded={true}
+                      placeholder={`Enter Your Email`}
+                    ></Input>
+                  </div>
+                </div>
+                <div className={`w-full max-w-two`}>
+                  <MainButton isRounded={true}>Subscribe</MainButton>
+                </div>
               </div>
             </div>
           </div>
