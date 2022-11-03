ac2941cb3d1895537e68c866811f9bcf14d4eb7f
Wilson Ibekason <wanbekgithub@gmail.com>
2022-11-03 22:27:38 +0100

    complete add Contents

--- src/components/shop/shopdetail/detailmain/Contents.tsx
+++ src/components/shop/shopdetail/detailmain/Contents.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import cx from "classnames";
 import { useIcon } from "../../../../hooks/dispatchContext";
+import MainButton from "../../../../hooks/button/mainBTN";
 import {
   flexLayout,
   themes,
@@ -37,12 +38,29 @@ const Contents = () => {
             </div>
           </div>
 
-          <div
-          className={`${}`}
-          
-          >
-
+          <div className={`${flexRowCenterBetween}`}>
+            <div></div>
+            <div
+              className={cx(
+                `w-14 h-7 px-2 py-1 border border-orange  rounded-md`
+              )}
+            >
+              <div className={cx(`${boxFull} ${flexCenter} flex space-x-1`)}>
+                <p>0</p>
+                <HeartIcon />
+              </div>
+            </div>
           </div>
+          {/*  */}
+          <div className={`${flexStart}`}>
+            <p>
+              The swordfish meat has a very delicate flavour, meaty and mild.
+              Swordfish, also known as broadbills, is an oily fish similar to
+              tuna, chunky meat without bones.
+            </p>
+          </div>
+          {/*  */}
+          <p className={cx(`text-start`)}>Read More</p>
         </div>
 
         {/*  */}
@@ -51,7 +69,35 @@ const Contents = () => {
           <p>$23</p>
         </div>
         {/*  */}
-        <div></div>
+        <div className={`${flexCol}`}>
+          {/*  */}
+          <div className={`${flexRowCenterBetween}`}>
+            <div className={`w-full max-w-full`}>
+              <MainButton isRounded={true}>Add To Shopping Cart</MainButton>
+            </div>
+            <p className={`whitespace-nowrap`}>9 items Remaining </p>
+          </div>
+          {/*  */}
+          <div className="w-full h-px bg-gray-900" />
+          {/*  */}
+          <div className={`${flexRowCenter}`}>
+            <p>Categories</p>
+            <div className={`${flexRowCenter}`}>
+              {Array(3)
+                .fill(0)
+                .map((i) => (
+                  <div
+                    key={i}
+                    className={`p-2 border border-orange rounded-md`}
+                  >
+                    <div className={`${flexCenter} ${boxFull}`}>
+                      <p>Rice and Beans</p>
+                    </div>
+                  </div>
+                ))}
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
