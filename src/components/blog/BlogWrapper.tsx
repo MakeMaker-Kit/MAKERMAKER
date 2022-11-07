63b593c16d3d2f4e94dbacae51c69589eb4319c7
wilsonibekason <wanbekgithub@gmail.com>
2022-11-07 16:48:59 +0100

    Add BannerPAGE wrapper to blog details

--- src/components/blog/BlogWrapper.tsx
+++ src/components/blog/BlogWrapper.tsx
@@ -20,9 +20,13 @@ const BlogWrapper: React.FC<Props> = ({ children }) => {
           <BlogHeader />
           <div className={`w-full h-px bg-gray-200 my-4`} />
           {/*  BnnaerPage Wrappers*/}
-          {/* <div>
-            <BannerPageWrapper />
-          </div> */}
+          <div>
+            <BannerPageWrapper
+              home="home"
+              routePath="Blog"
+              routePathID="How TO Be a good Man"
+            />
+          </div>
         </div>
         <AuthLayout />
         <div className={`${pageCenter} bg-white`}>
