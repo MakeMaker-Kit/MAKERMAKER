ee25038ceaf53afb63235418386e0b94790fc7f6
Wilson Ibekason <wanbekgithub@gmail.com>
2022-11-09 12:49:16 +0100

    Add Links locations to the wrapper links

--- src/components/appwrapper/bannerPageWrapper/BannerPageWrapper.tsx
+++ src/components/appwrapper/bannerPageWrapper/BannerPageWrapper.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { flexLayout, textStyles, themes } from "../../../styles/themes/theme";
 import classNames from "classnames";
+import { Link, useNavigate } from "react-router-dom";
 type BannerProps = Omit<React.ComponentProps<"div">, "className"> & {};
 interface MoreProps extends BannerProps {
   children?: string | undefined;
@@ -23,20 +24,25 @@ const BannerPageWrapper = React.forwardRef<HTMLDivElement, MoreProps>(
               <div
                 className={`${boxFull} ${flexCenter} ${flexRow} space-x-3 text-white text-xs  ${textCustom}`}
               >
-                <span className={`capitalize cursor-pointer hover:underline`}>
+                <Link
+                  to={`/`}
+                  className={`capitalize cursor-pointer hover:underline`}
+                >
                   {home ? home : "Home"}
-                </span>
+                </Link>
                 {routePath && <span>/</span>}
-                <span
+                <Link
+                  to={`/${routePath}`}
                   className={classNames(
                     `capitalize cursor-pointer hover:underline`,
                     `${!routePathID ? "text-gray-900" : ""}`
                   )}
                 >
                   {routePath ? routePath : ""}
-                </span>
+                </Link>
                 {routePathID && <span> / </span>}
-                <span
+                <Link
+                  to={`/${routePath}/${routePathID}`}
                   className={classNames(
                     `${
                       routePath && routePathID ? "text-gray-900" : ""
@@ -44,7 +50,7 @@ const BannerPageWrapper = React.forwardRef<HTMLDivElement, MoreProps>(
                   )}
                 >
                   {routePathID ? routePathID : ""}
-                </span>
+                </Link>
               </div>
             </div>
           </div>
